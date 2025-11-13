720bc21f6d38976420af23c9befdf3e0ba9e1259
Evianna <115055847+eviannaive@users.noreply.github.com>
2025-11-13 19:36:40 +0800

    update

--- example/next.config.ts
+++ example/next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   reactCompiler: true,
+  basePath: "/react-secret-input",
+  assetPrefix: "/react-secret-input",
 };
 
 export default nextConfig;
